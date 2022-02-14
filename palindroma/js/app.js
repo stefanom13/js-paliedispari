8d0de0e8aedbf5746d123554868bcd0d61474fd2
stefanom13 <stefanomancuso13@gmail.com>
2022-02-14 17:21:50 +0100

    inserimento senza funzione

--- palindroma/js/app.js
+++ palindroma/js/app.js
@@ -1,3 +1,26 @@
-// Palidroma
+// Palindroma
+
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+
+let word = prompt('inserisci una parola').toLowerCase();
+console.log (word)
+
+const parolaScomposta = [...word]
+console.log (parolaScomposta)
+
+const wordInverted = parolaScomposta.reverse();
+console.log (wordInverted)
+
+const wordJoin = parolaScomposta.join('')
+console.log (wordJoin)
+
+if (wordJoin === word) {
+
+    alert('la parola è palindroma')
+    
+} else {
+    alert('la parola non è palindroma')
+}
+
